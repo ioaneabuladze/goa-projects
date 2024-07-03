f5c2441aa065c5942ce5a5d04cb1bf5d9d1eba01
ioaneabuladze <vane.abuladze@gmail.com>
2024-07-03 07:56:32 -0700

    second commit

--- level 00/homework/lesson.py
+++ level 00/homework/lesson.py
@@ -2,8 +2,47 @@ from turtle import*
 
 # we want to draw a house
 
-step 1: we want to draw a house
+#step 1: draw a sqare
+width(8)
+color('blue')
+begin_fill()
+forward(200)
+left(90)
 
 forward(200)
 left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+end_fill()
+#end of square
+
+#drawing a door
+
+forward(70)
+color("yellow")
+begin_fill()
+left(90)
+forward(120)
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+
+
+penup()
+goto(200, 200)
+pendown()
+
+color("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
 
