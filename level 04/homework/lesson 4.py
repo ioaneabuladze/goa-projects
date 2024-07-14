362cedb9e3580a5c40ba87df51e56575d69b08e7
ioaneabuladze <vane.abuladze@gmail.com>
2024-07-14 10:18:22 -0700

    first commit

---
+++ level 04/homework/lesson 4.py
@@ -0,0 +1,36 @@
+
+
+dog_name = "ioane"
+
+print(dog_name)
+
+# shevqmenit 2 sityviani cvladi mivaniwet stringi da shemdeg davprintet 
+
+print(5+5)
+print(10-2)
+print(5*2)
+print(20/2)
+print(20//2)
+
+num = 20
+num1 = 10 
+print(num + num1)
+print(num - num1)
+print(num * num1)
+print(num / num1)
+
+
+
+# swori pasuxia (b)
+# simboloebi ertmanets unda sheesabamebodes
+# kodis shemowmeba,kodis dawera da gashveba,kodis dawera da dakomentareba,
+
+
+
+
+
+
+
+
+
+
