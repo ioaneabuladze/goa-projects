a6660d924f039e38d9e696f4232c431303d2ed91
ioaneabuladze <vane.abuladze@gmail.com>
2024-08-04 05:37:50 -0700

    second commit

---
+++ level 06/homework/lesson.py
@@ -0,0 +1,14 @@
+name = input(("enter your name: "))
+lastname = input(("enter your lastname: "))
+# print('your fullname is' + name + ' ' + lastname )
+print(f"your fullname is {name} {lastname}")
+# print('hello' + "name")
+name = input(('enter your name:' ))
+print(f"hello {name}")
+ 
+
+
+
+
+#output aris kompiuteridan gamomavali informacia,input aris kompiutershi shemavali informacia
+#integer aris ricxvi romelic tavstdeba cvladshi,float aris atwiladi,da string aris asoebis gan shemdgai sityva romelic tavstdeba cvladshi
