64e23c8771064013b7877ebe64040501373925d3
ioaneabuladze <vane.abuladze@gmail.com>
2024-08-28 11:20:33 -0700

    second commit

---
+++ level 10/homework/lesson.py
@@ -0,0 +1,112 @@
+city1= ("tbilisi")
+city2 = ("qutaisi")
+city3 = ("svaneti")
+print(city2)
+print(city3)
+print(city1)
+
+
+name = ("ioane")
+lastname = ("abuladze")
+print(name + " " + lastname)
+
+num1 = (4)
+num2 = (5)
+print( num1 + num2)
+print(num2 - num1)
+print(num1 * num2)
+print(num2 / num1)
+
+
+
+a = (5)
+B = (10)
+a = B
+B = a 
+print(a)
+print(B)
+
+
+
+
+color = ("what is your favorite color? ")
+print(" my all time favorite color is "+ " " + color)
+
+
+name1 = ("jonny")
+age = (14)
+hight = (15.4)
+print(type(name1))
+
+
+
+
+float = (1.3)
+int = int(float)
+print(int)
+print(float)
+
+
+
+int=(134)
+print(type(int))
+
+
+num1 = (14)
+num2 = (15)
+plus = num1 + num2
+minus = num2 - num1
+multiply = num1 * num2
+division = num2 / num1
+print(plus)
+print(minus)
+print(multiply)
+print(division)
+
+
+
+
+string1 = ('hi ')
+string2 = ('everyone ')
+string3 = (" how are yall ")
+full_string = string1 + string2 + string3
+print(full_string)
+
+
+
+num1 = (8)
+num2 = (3)
+print(num1<num2)
+print(num1>num2)
+print(num1<=num2)
+print(num1>=num2)
+print(num1==num2)
+
+
+
+
+num1 = (14)
+num2 = (15)
+print(num1>=num2)
+
+
+
+num1 = (45)
+num2 = (34)
+num3 = (23)
+print(num1<num2)
+print(num2<num3)
+
+
+
+num1 = (9)
+num2 = (5)
+
+print(num1==num2)
+print(num1!=num2)
+
+
+
+
+
+
