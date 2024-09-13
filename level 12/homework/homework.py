8c168ddfd5be44ca705f763d225c8950e1b830ca
ioaneabuladze <vane.abuladze@gmail.com>
2024-09-13 07:43:12 -0700

    second commit

---
+++ level 12/homework/homework.py
@@ -0,0 +1,35 @@
+for i in range(7):
+    print(10)
+
+for i in range(8):
+    print("HELLO")
+
+
+#name = input('enter your name')
+#for i in range(5):
+    #print('hello'  + name)
+
+a = 10
+b = 30
+while a <= b:
+    a=a+1
+    print(a)
+
+a = 50
+b = 10
+while a>=b:
+    a=a-1
+    print(a)
+
+
+c = 0
+g = 10
+
+while c<=g:
+    c=c+1
+    print("goa best")
+    
+
+    
+
+
